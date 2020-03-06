7add7b5050b3ff4815c76a4649d70254642f1941
rajatprototype <rajat.prototype@gmail.com>
2020-03-06 16:44:57 +0530

    Updated manifest Added app icon, favicon

---
+++ src/components/LoadingBar.vue
@@ -0,0 +1,71 @@
+<template>  
+  <div class="loader">
+    <svg class="circular" viewBox="25 25 50 50">
+      <circle 
+        class="path" 
+        cx="50" 
+        cy="50" 
+        r="20" 
+        fill="none" 
+        stroke-width="4" 
+        stroke-miterlimit="10" />
+    </svg>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'loader-bar'
+  }
+</script>
+
+<style scoped>
+  .loader {
+    position: relative;
+    margin: 0 auto;
+    width: 3.4em;
+  }
+  .loader:before {
+      content: '';
+      display: block;
+      padding-top: 100%;
+  }
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -35px;
+    }
+    100% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -124px;
+    }
+  }
+  .circular {
+    stroke: var(--pcolor);
+    animation: rotate 1.4s linear infinite;
+    height: 100%;
+    transform-origin: center center;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+  .path {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+    animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out infinite;
+    stroke-linecap: round;
+  }
+</style>
--- src/views/Peoples.vue
+++ src/views/Peoples.vue
@@ -10,19 +10,28 @@
         <h4>Peoples</h4>
       </header>
     </div>
-    <div class="data-list">
-      <router-link 
-        tag="div" 
-        to="/users" 
-        class="person"
-        v-for="(person, index) in datalist" 
-        :key="index">
-        <avatar :src="person.photoURL" :uname="person.displayName" />
-        <div class="detail">
-          <h4>{{ person.displayName }}</h4>
-          <p><small>{{ person.email }}</small></p>
-        </div>
-      </router-link>
+    <div class="container">
+      <div class="data-list" v-if="datalist.length > 1">
+        <router-link 
+          tag="div" 
+          to="/users" 
+          class="person"
+          v-for="(person, index) in datalist" 
+          :key="index">
+          <avatar :src="person.photoURL" :uname="person.displayName" />
+          <div class="detail">
+            <h4>{{ person.displayName }}</h4>
+            <p><small>{{ person.email }}</small></p>
+          </div>
+        </router-link>
+      </div>
+      <div v-else-if="loadingStatus && datalist.length == 0" class="loader-cover">
+        <loading-bar />
+      </div>
+      <div v-else class="data-empty-error">
+        <div class="icon"><frown-icon /></div>
+        <h4>No anyone found</h4>
+      </div>
     </div>
   </div>
 </template>
@@ -32,6 +41,8 @@
   import ActionIcon from '@/components/ActionIcon'
   import router from '@/router'
   import Avatar from '@/components/Avatar'
+  import { FrownIcon } from 'vue-feather-icons'
+  import LoadingBar from '../components/LoadingBar'
   import { firestore } from '../firebase-init'
 
   export default {
@@ -40,20 +51,28 @@
     components: {
       'arrow-left-icon': ArrowLeftIcon,
       'action-icon': ActionIcon,
-      'avatar': Avatar
+      'avatar': Avatar,
+      'frown-icon': FrownIcon,
+      'loading-bar': LoadingBar
     },
     data() {
       return {
+        loadingStatus: true, 
         datalist: []
       }
     },
     created() {
       firestore.collection('userdata').get().then(snapshot => {
         const { docs } = snapshot;
-        docs.forEach(doc => {
-          this.datalist.push(doc.data());
-        })
-      });
+        if (docs.length) {
+          docs.forEach(doc => {
+            this.datalist.push(doc.data());
+          })
+          this.loadingStatus = false;
+        } else {
+          this.loadingStatus = false;
+        }
+      }).catch(() => this.loadingStatus = false);
     },
     methods: {
       back() {
@@ -64,6 +83,11 @@
 </script>
 
 <style>
+  .peoples {
+    display: grid;
+    grid-template-rows: 4.5em auto;
+    height: inherit;
+  }
   .peoples .topbar {
     padding: 8px var(--base-margin);
     display: flex;
@@ -71,23 +95,23 @@
     border-bottom: 1px solid var(--front-fg-minor-blur);
     box-shadow: 0 1px 3px var(--front-fg-minor-blur);
   }
-  .peoples .data-list .person {
+  .peoples .container .data-list .person {
     display: flex;
     align-items: center;
     padding: 12px 1em;
   }
-  .peoples .data-list .person .detail {
+  .peoples .container .data-list .person .detail {
     padding: 0 12px;
   }
-  .peoples .data-list .person .detail h4 {
+  .peoples .container .data-list .person .detail h4 {
     margin: 0;
     padding: 1px;
   }
-  .peoples .data-list .person p {
+  .peoples .container .data-list .person p {
     margin: 0;
     padding: 1px;
   }
-  .peoples .data-list .person .avatar {
+  .peoples .container .data-list .person .avatar {
     width: 3.3em;
     height: 3.3em;
   }
